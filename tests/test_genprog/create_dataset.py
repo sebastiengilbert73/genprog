3c2d97db714f7dd9fe53b73d2f7d88e4b60db7ce
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2020-04-13 12:32:13 -0400

    create_dataset.py: Added functions CreateDataset_2D() and EvaluateIndividuals()

--- tests/test_genprog/create_dataset.py
+++ tests/test_genprog/create_dataset.py
@@ -1,6 +1,8 @@
 from typing import Dict, List, Tuple, Any, Optional, Set, Union
 import random
 import math
+import logging
+from genprog import core as gp, evolution as gpevo
 
 
 def CreateDataset(prototype: str, numberOfSamples: int) -> List[Tuple[Dict[str, float], float]]:
@@ -14,6 +16,24 @@ def CreateDataset(prototype: str, numberOfSamples: int) -> List[Tuple[Dict[str, 
             outputValue = math.sin(2 * math.pi * (x))
         elif prototype == 'parabola':
             outputValue = 6 * x**2 - 5 * x + 0.4
+        else:
+            raise NotImplementedError("CreateDataset(): Not implemented prototype '{}'".format(prototype))
+        xDictOutputValueTupleList.append((xToValuesDict, outputValue))
+
+    return xDictOutputValueTupleList
+
+def CreateDataset_2D(prototype: str, numberOfSamples: int) -> List[Tuple[Dict[str, float], float]]:
+    xDictOutputValueTupleList: List[Tuple[Dict[str, float], float]] = []
+
+    for sampleNdx in range(numberOfSamples):
+        x: float = random.random()
+        y: float = random.random()
+        xToValuesDict: Dict[str, float] = {'x': x, 'y': y}
+        outputValue: float = 0
+        if prototype == 'sin2d':
+            outputValue = math.sin(2 * math.pi * x + 0.7 * math.pi * y - 0.4)
+        elif prototype == 'parabola2d':
+            outputValue = 6 * x**2 - 5 * x + 0.4 - 2.5 * y**2 + 1.7 * y + 2.1 * x * y
         else:
             raise NotImplementedError("CreateDataset(): Not implemented prototype '{}'".format(prototype))
         xDictOutputValueTupleList.append((xToValuesDict, outputValue))
@@ -36,3 +56,36 @@ def SplitDataset(datasetList: List[Tuple[Dict[str, float], float]], trainingProp
         else:
             validationDataset.append((xyDict, value))
     return (trainingDataset, validationDataset)
+
+def EvaluateIndividuals(population: gpevo.ArithmeticsPopulation,
+                         trainingDataset: List[Tuple[Dict[str, Union[float, bool]], float]],
+                         validationDataset: List[Tuple[Dict[str, Union[float, bool]], float]],
+                         variableNameToTypeDict: Dict[str, str],
+                         interpreter: gp.ArithmeticsInterpreter,
+                         returnType: str
+                         ) -> Tuple[gp.Individual, float, float, float, float, Dict[gp.Individual, float]]:
+    logging.info("Evaluating the individuals...")
+    training_individualToCostDict: Dict[gp.Individual, float] = population.EvaluateIndividualCosts(
+        trainingDataset,
+        variableNameToTypeDict,
+        interpreter,
+        returnType
+    )
+
+    (trainingChampion, championTrainingCost) = population.Champion(training_individualToCostDict)
+    medianTrainingCost: float = population.MedianCost(training_individualToCostDict)
+
+    # Validation cost
+    validation_individualToCostDict: Dict[gp.Individual, float] = population.EvaluateIndividualCosts(
+        validationDataset,
+        variableNameToTypeDict,
+        interpreter,
+        returnType
+    )
+    (validationChampion, championValidationCost) = population.Champion(validation_individualToCostDict)
+    medianValidationCost = population.MedianCost(validation_individualToCostDict)
+
+    logging.info(
+        "championTrainingCost = {};    championValidationCost = {};    medianTrainingCost = {};    medianValidationCost = {}".format(
+            championTrainingCost, championValidationCost, medianTrainingCost, medianValidationCost))
+    return (validationChampion, championTrainingCost, championValidationCost, medianTrainingCost, medianValidationCost, training_individualToCostDict)
