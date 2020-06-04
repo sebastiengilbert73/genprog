ad578cccdd61dfe02162814ad93385ffd0038bd3
Sébastien Gilbert <sebastiengilbert73@yahoo.ca>
2020-06-03 20:14:48 -0400

    create_1d_dataset.py: Generate train, validation and test data

--- tutorials/create_1d_dataset.py
+++ tutorials/create_1d_dataset.py
@@ -10,11 +10,17 @@ def function(x):
     f3 = gaussian(x, 0.9, 0.4)
     return 0.4 * f1 - 0.5 * f2 + 0.1 * f3
 
+def write_to_file(data, filepath):
+    with open(filepath, "w") as outputFile:
+        outputFile.write('x,y\n')
+        for (x, y) in data:
+            outputFile.write("{},{}\n".format(x, y))
+
 def main():
     print ("create_1d_dataset.py main()")
     numberOfSamples = 2000
     noiseAmplitude = 0.02
-    outputFilepath = './data/samples_1d.csv'
+    outputFilepathPrefix = './data/samples_1d'
 
     rng = numpy.random.default_rng()
     xs = rng.random(numberOfSamples)
@@ -23,11 +29,13 @@ def main():
     ys += noises
     xys = list(zip(xs, ys))
 
-    # Write to a file
-    with open(outputFilepath, "w") as outputFile:
-        outputFile.write('x,y\n')
-        for (x, y) in xys:
-            outputFile.write("{},{}\n".format(x, y))
+    # Write to files
+    train_xys = xys[: int(0.6 * numberOfSamples)]
+    validation_xys = xys[int(0.6 * numberOfSamples): int(0.8 * numberOfSamples)]
+    test_xys = xys[int(0.8 * numberOfSamples):]
+    write_to_file(train_xys, outputFilepathPrefix + '_train.csv')
+    write_to_file(validation_xys, outputFilepathPrefix + '_validation.csv')
+    write_to_file(test_xys, outputFilepathPrefix + '_test.csv')
 
 
 
